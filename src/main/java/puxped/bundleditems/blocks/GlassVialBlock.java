8c735c7f468bb6f6a6717648c00b62172cea5842
puxped <ktv6055@gmail.com>
2024-03-17 14:02:21 +0100

    GlassVial Voxel Shape

--- src/main/java/puxped/bundleditems/blocks/GlassVialBlock.java
+++ src/main/java/puxped/bundleditems/blocks/GlassVialBlock.java
@@ -2,7 +2,11 @@ package puxped.bundleditems.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class GlassVialBlock extends Block {
@@ -17,6 +21,15 @@ public class GlassVialBlock extends Block {
     @Override
     public boolean isTransparent(BlockState state, BlockView world, BlockPos pos) {
         return true;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        VoxelShape shape = VoxelShapes.empty();
+        shape = VoxelShapes.combineAndSimplify(shape, VoxelShapes.cuboid(0.375, 0, 0.375, 0.6875, 0.5, 0.6875), BooleanBiFunction.OR);
+        shape = VoxelShapes.combineAndSimplify(shape, VoxelShapes.cuboid(0.4375, 0.5, 0.4375, 0.625, 0.5625, 0.625), BooleanBiFunction.OR);
+
+        return shape;
     }
     
 }
