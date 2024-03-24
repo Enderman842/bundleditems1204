9724083fd9f545e957d084122b582bde73ea2c6e
puxped <ktv6055@gmail.com>
2024-03-24 12:49:08 +0100

    24.03.2024a

--- src/main/java/puxped/bundleditems/BIBlocks.java
+++ src/main/java/puxped/bundleditems/BIBlocks.java
@@ -11,9 +11,17 @@ import puxped.bundleditems.blocks.GlassVialBlock;
 public class BIBlocks {
 
     public static final Block GLASS_VIAL = new GlassVialBlock(FabricBlockSettings.copy(Blocks.GLASS));
+    public static final Block BUNDLED_GLOWSTONE_DUST = new GlassVialBlock(FabricBlockSettings.create().luminance(8));
+    public static final Block BUNDLED_GUNPOWDER = new GlassVialBlock(FabricBlockSettings.copy(Blocks.GLASS));
+    public static final Block BUNDLED_REDSTONE = new GlassVialBlock(FabricBlockSettings.copy(Blocks.GLASS));
+    public static final Block BUNDLED_SUGAR = new GlassVialBlock(FabricBlockSettings.copy(Blocks.GLASS));
     
     public static void regBlocks() {
         registerBlock("small_bottle", GLASS_VIAL);
+        registerBlock("bundled_glowstone_dust", BUNDLED_GLOWSTONE_DUST);
+        registerBlock("bundled_gunpowder", BUNDLED_GUNPOWDER);
+        registerBlock("bundled_redstone", BUNDLED_REDSTONE);
+        registerBlock("bundled_sugar", BUNDLED_SUGAR);
     }
 
     public static void registerBlock(String blockName, Block block) {
